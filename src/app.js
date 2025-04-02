03e7409bebdf4e95f9aacbf0ba9d5a5775a0466d
gcruzbull <153864295+gcruzbull@users.noreply.github.com>
2025-04-02 20:49:35 +0000

    generador de dominios en consola y página web más otros detalles

--- src/app.js
+++ src/app.js
@@ -8,4 +8,27 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let domains = getDomains()
+  let list = document.getElementById("domains")
+  domains.forEach(element => {
+    list.innerHTML += `<li>${element}</li>`
+  });
 };
+
+
+function getDomains() {
+  let name = ['guillermo', 'alvaro', 'laura', 'valentina', 'deimian', 'julian', 'tomas', 'maria', 'anita'];
+  let lastname = ['cruz', 'bull', 'barros', 'alvarez', 'lira', 'valenzuela', 'vidal', 'perez'];
+  let number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let newArray = []
+  for (let i = 0; i < name.length; i++) {
+    console.log("Hello!")
+    for (let j = 0; j < lastname.length; j++) {
+      for (let k = 0; k < number.length; k++ ) {
+        console.log(`${name[i]}${lastname[j]}${number[k]}.com`)
+        newArray.push(`${name[i]}${lastname[j]}${number[k]}.com`)
+      }
+    }
+  }
+  return newArray
+}
